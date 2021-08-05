eed6cfe4ed189904a0c7c19aa6501a48cc9ec69b
Animesh <er.animesh6@hotmail.com>
2021-08-05 20:34:42 +0530

    first test case.

--- Request_Automation/test_get_request.py
+++ Request_Automation/test_get_request.py
@@ -9,5 +9,15 @@ print(status_code)
 assert status_code == 200, "Status code is not 200."
 print(resp.text)  # Content of the response, in unicode.
 print(resp.content)  # Content of the response, in bytes.
-print(resp.json())  # Returns the json-encoded content of a response, if any
+resp_json = resp.json()
+resp_header = resp.headers
+resp_cookies = resp.cookies
+resp_encoding = resp.encoding
+print(resp_header)  # returns all the headers
+print(resp_json)  # Returns the json-encoded content of a response, if any
 # Navigate to http://jsonviewer.stack.hu/ to view the json format data
+print(resp_cookies)
+
+#   *** Verify response ***
+
+print(resp_json['total_pages'])
