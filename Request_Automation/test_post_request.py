3afae20eb636840a822a01394af223d4a3517c2c
Animesh <er.animesh6@hotmail.com>
2021-08-05 21:04:26 +0530

    second test case.

--- Request_Automation/test_post_request.py
+++ Request_Automation/test_post_request.py
@@ -7,4 +7,5 @@ status_code = resp.status_code
 resp_json = resp.json()
 print(status_code)
 print(resp_json)
+print(resp.headers.get("Content-Type"))  # this is to get the content type filed from header
 assert resp_json['job'] == 'QA Engineer', "Job title is not matching with the expected job title"
