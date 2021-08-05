924d94ff0a2a79cc6a4078b07feacc99b34c4a5c
Animesh <er.animesh6@hotmail.com>
2021-08-05 20:54:51 +0530

    second test case.

--- Request_Automation/test_post_request.py
+++ Request_Automation/test_post_request.py
@@ -1,7 +1,12 @@
 import requests
 
-data = {
-    "name": "morpheus",
-    "job": "leader"
+payload = {
+    "name": "Animesh",
+    "job": "QA engineer"
 }
-resp = requests.post("https://reqres.in/api/users")
+
+resp = requests.post("https://reqres.in/api/users", data=payload)
+status_code = resp.status_code
+resp_json = resp.json()
+print(status_code)
+print(resp_json)
