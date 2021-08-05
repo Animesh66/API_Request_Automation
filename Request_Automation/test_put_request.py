ece82b78a7967b6898c3fa8587cf176db6ab8cfa
Animesh <er.animesh6@hotmail.com>
2021-08-05 22:03:40 +0530

    second test case.

--- Request_Automation/test_put_request.py
+++ Request_Automation/test_put_request.py
@@ -1,6 +1,10 @@
 import requests
+
 payload = {
-    "name": "morpheus",
-    "job": "zion resident"
+    "name": "Nitai",
+    "job": "Melbourne"
 }
+
 resp = requests.put('https://reqres.in/api/users/2', data=payload)
+print(resp.status_code)
+print(resp.json())
