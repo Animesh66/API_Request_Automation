d89fba6c0caeaafbcd48cc86bc71ca940864241d
Animesh <er.animesh6@hotmail.com>
2021-08-05 22:10:08 +0530

    second test case.

--- Request_Automation/test_delete_user.py
+++ Request_Automation/test_delete_user.py
@@ -1,5 +1,5 @@
 import requests
 
-resp = requests.delete("https://reqres.in/api/users/2")
+resp = requests.delete("https://reqres.in/api/users/200")
 print(resp.status_code)
 assert resp.status_code == 204, "Response code should be 204 but not matching"
